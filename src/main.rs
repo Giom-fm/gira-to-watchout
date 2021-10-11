8cb569543d756b31c8a044958963c3f9286c9b89
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2021-10-11 18:37:03 +0200

    first mvp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::http::Status;
+use rocket::Request;
+use std::sync::{Arc, Mutex};
+use telnet::{Event, Telnet};
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
+}
+
+#[get("/run/<task>")]
+fn run(task: String) -> rocket::http::Status {
+    let result = Telnet::connect(("127.0.0.1", 30100), 256);
+    let mut connection = match result {
+        Ok(res) => res,
+        Err(_) => return Status::InternalServerError,
+    };
+
+    let command = format!("authenticate 1\nrun {}", task);
+    connection.write(command.as_bytes()).unwrap();
+    Status::NoContent
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .register("/", catchers![not_found])
+        .mount("/", routes![run])
+        .launch()
+        .await;
+
+    /*loop {
+        let event = connection.read_nonblocking().expect("Read error");
+        if let Event::Data(buffer) = event {
+            // Debug: print the data buffer
+            println!("{:?}", buffer);
+            // process the data buffer
+        }
+    }*/
 }
