d97d8b166f31306b76c4c28804b3fd963522a14b
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2021-10-12 18:20:00 +0200

    Split send into functions

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,36 @@ use telnet::{Event, Telnet};
 const TELNET_HOST: &str = "192.168.1.11";
 const TELNET_PORT: u16 = 3039;
 
+
+fn authenticate(connection: &mut telnet::Telnet) -> Result<(), ()> {
+    if let Err(_) = send(connection, "authenticate 1\n") {
+        return Err(());
+    }
+
+    loop {
+        let result = connection.read_nonblocking();
+        let event = match result {
+            Ok(event) => event,
+            Err(_) => return Err(()),
+        };
+
+        if let Event::Data(buffer) = event {
+            let response = String::from_utf8_lossy(&buffer);
+            println!("{:?}", response);
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn send(connection: &mut telnet::Telnet, command: &str) -> Result<(), ()> {
+    if let Err(_) = connection.write(command.as_bytes()) {
+        return Err(());
+    };
+    Ok(())
+}
+
+
 #[catch(404)]
 fn not_found(req: &Request) -> String {
     format!("Sorry, '{}' is not a valid path.", req.uri())
@@ -21,45 +51,18 @@ fn run(task: String) -> rocket::http::Status {
         Err(_) => return Status::InternalServerError,
     };
 
-   connection.write("authenticate 1\n".as_bytes()).unwrap();
-    loop {
-        let result = connection.read_nonblocking();
-        let event = match result {
-            Ok(res) => res,
-            Err(_) => return Status::InternalServerError,
-        };
-
-        if let Event::Data(buffer) = event {
-            let response = String::from_utf8_lossy(&buffer);
-            println!("{:?}", response);
-            break;
-        }
+    if let Err(_) = authenticate(&mut connection) {
+        return Status::InternalServerError;
     }
-    connection
-        .write(format!("run {}\n", task).as_bytes())
-        .unwrap();
+
+    if let Err(_) = send(&mut connection, &format!("run {}\n", task)) {
+        return Status::InternalServerError;
+    };
+
     Status::NoContent
 }
-/*
-fn authenticate(connection: telnet::Telnet) -> Result<(), ()> {
-    connection.write("authenticate 1\n".as_bytes()).unwrap();
-    loop {
-        let result = connection.read_nonblocking();
-        let event = match result {
-            Ok(res) => res,
-            Err(_) => return Err(()),
-        };
 
-        if let Event::Data(buffer) = event {
-            let response = String::from_utf8_lossy(&buffer);
-            println!("{:?}", response);
-            break;
-        }
-    }
-    Ok(())
-}*/
-
-#[get("/halt")]
+#[get("/kill")]
 fn halt() -> rocket::http::Status {
     let result = Telnet::connect((TELNET_HOST, TELNET_PORT), 256);
     let mut connection = match result {
@@ -67,20 +70,14 @@ fn halt() -> rocket::http::Status {
         Err(_) => return Status::InternalServerError,
     };
 
-    connection.write("authenticate 1\n".as_bytes()).unwrap();
-    loop {
-        let result = connection.read_nonblocking();
-        let event = match result {
-            Ok(res) => res,
-            Err(_) => return Status::InternalServerError,
-        };
-        if let Event::Data(buffer) = event {
-            let response = String::from_utf8_lossy(&buffer);
-            println!("{:?}", response);
-            break;
-        }
+    if let Err(()) = authenticate(&mut connection) {
+        return Status::InternalServerError;
     }
-    connection.write("halt\n".as_bytes()).unwrap();
+
+    if let Err(_) = send(&mut connection, "kill\n") {
+        return Status::InternalServerError;
+    };
+
     Status::NoContent
 }
 
